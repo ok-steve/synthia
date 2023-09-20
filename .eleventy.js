742d39fe24e6d5af270ef067e64b9dd1629a18a1
Steve Cherry <steve@stevecherry.net>
2023-09-20 10:56:59 -0400

    Add bundle plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,17 @@
 const { EleventyHtmlBasePlugin } = require('@11ty/eleventy');
 const EleventyNavigation = require('@11ty/eleventy-navigation');
+const EleventyBundlePlugin = require('@11ty/eleventy-plugin-bundle');
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
   eleventyConfig.addPlugin(EleventyNavigation);
+  eleventyConfig.addPlugin(EleventyBundlePlugin);
 
   eleventyConfig.setServerPassthroughCopyBehavior('passthrough');
   eleventyConfig.addPassthroughCopy('./public');
   eleventyConfig.addPassthroughCopy('./src/sw.js');
+
+  eleventyConfig.addWatchTarget('./src/**/*.{css,js}');
 
   return {
     dir: {
